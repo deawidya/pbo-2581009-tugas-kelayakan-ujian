9615d68b964084a449ac899b329d7e1b6ef4d3d1
Dea Widya Ningsih <2581009@unai.edu>
2026-09-25 10:28:07 +0700

    Tambah tiga versi syarat kelayakan (a, b, c) dan negasi dispensasi

--- src/kelayakanUjian.java
+++ src/kelayakanUjian.java
@@ -13,11 +13,22 @@ public class kelayakanUjian {
         System.out.print("Dispensasi      : ");
         boolean dispensasi = input.nextBoolean();
 
+        boolean a = kehadiran >= 75 && nilaiTugas >= 60 || dispensasi;
+        boolean b = (kehadiran >= 75 && nilaiTugas >= 60) || dispensasi;
+        boolean c = kehadiran >= 75 && (nilaiTugas >= 60 || dispensasi);
+
+        boolean tidakDispensasi = !dispensasi;
+
         System.out.println();
         System.out.println("===== KELAYAKAN UJIAN =====");
         System.out.println("Kehadiran   : " + kehadiran + "%");
         System.out.println("Nilai tugas : " + nilaiTugas);
         System.out.println("Dispensasi  : " + dispensasi);
+        System.out.println();
+        System.out.println("a (tanpa kurung)      : " + a);
+        System.out.println("b (kurung precedence) : " + b);
+        System.out.println("c (kurung digeser)    : " + c);
+        System.out.println("!dispensasi           : " + tidakDispensasi);
 
         input.close();
     }
